27633a3a5feb5f1c737cbcf457f4a523c7d517b3
Chazz Michaels <michaelchasedev@gmail.com>
2018-06-24 15:32:51 -0400

    Refactor message_params to be held inside private method in messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,10 @@
 class MessagesController < ApplicationController
+
   def new
     @message = Message.new
   end
+
   def create
-    message_params = params.require(:message).permit(:name, :email, :body)
     @message = Message.new message_params
 
     if @message.valid?
@@ -12,4 +13,11 @@ class MessagesController < ApplicationController
       render :new
     end
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:name, :email, :body)
+  end
+
 end
