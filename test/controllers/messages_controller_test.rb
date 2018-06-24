628a3a2ded0bd20f59a4859a8cf8e0b3900772cf
Chazz Michaels <michaelchasedev@gmail.com>
2018-06-24 15:29:44 -0400

    Update 'create' method in messages_controller to only redirect and follow through with submit action if the fields are valid, otherwise stay on the existing page, write corresponding test in messages_controller_test.rb

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -31,4 +31,14 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
     assert_match /Message received, thanks!/, response.body
   end
 
+  test "invalid POST create" do
+    post create_message_url, params: {
+      message: { name: '', email: '', body: '' }
+    }
+
+    assert_match /Name .* blank/, response.body
+    assert_match /Email .* blank/, response.body
+    assert_match /Body .* blank/, response.body
+  end
+
 end
